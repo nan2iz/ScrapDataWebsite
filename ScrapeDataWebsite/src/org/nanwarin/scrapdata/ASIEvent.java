6740acf1649e7bd50cee3f1eb5872c6dbf67be24
Nanwarin Chantarutai <nan2iz.ch@gmail.com>
2017-01-12 08:59:18 -0800

    update

---
+++ ScrapeDataWebsite/src/org/nanwarin/scrapdata/ASIEvent.java
@@ -0,0 +1,18 @@
+package org.nanwarin.scrapdata;
+
+public class ASIEvent {
+
+	
+	//view-source:http://www.asi.calpoly.edu/events/schedule
+	//var events = [
+	public static void parseDocument(){
+		
+		
+		
+	}
+	
+	
+	//pull data from Graph API
+	
+	
+}
